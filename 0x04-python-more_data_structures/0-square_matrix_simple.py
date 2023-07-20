1d02cc94bf86ec7519a271f10ea76234002c83bc
Yasminbakr <yasminbakr2019@gmail.com>
2023-07-19 18:00:16 -0700

    d

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+#return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
+
     for i in matrix:
         for j in matrix:
-            b = [[j ** 2 for j in i] for i in matrix]
-            return(b)
+            return[[j ** 2 for j in i] for i in matrix]
+        
